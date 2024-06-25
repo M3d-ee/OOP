5cd8784d47d340fdaf9569632ecec721cc0b51b3
Mouad <mouadelkhou4@gmail.com>
2024-06-26 00:20:27 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,8 @@
+class Mammal:
+    def __init__(self, name, age):
+        self.name = name  
+        self.age = age  
+    def make_sound(self):  
+        print("Makes sound")
+    def move(self):  
+        print("Moves")
