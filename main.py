0288b802aacbc8919fe8076f7571d266fbb1d1b4
Mouad <mouadelkhou4@gmail.com>
2024-06-26 23:08:54 +0200

    -the internal state of the class cannot be  modified directly -added two classes and two methods within these classes

--- main.py
+++ main.py
@@ -1,8 +1,35 @@
 class Mammal:
     def __init__(self, name, age):
-        self.name = name  
-        self.age = age  
-    def make_sound(self):  
-        print("Makes sound")
-    def move(self):  
-        print("Moves")
+        self._name = name  
+        self._age = age  
+    def make_sound(self):
+       return
+    def move(self):
+       return 
+    
+class Dog(Mammal):
+    def __init__(self, name, age, breed):
+        super().__init__(name, age)
+        self._breed = breed
+    def make_sound(self):
+        print(f"{self._name} : Woof ")
+    def move(self):
+        print(f"{self._name} is running")
+
+class Cat(Mammal):
+    def __init__(self, name, age, color):
+        super().__init__(name, age)
+        self._color = color
+    def make_sound(self):
+        print(f"{self._name} : Meow")
+    def move(self):
+        print(f"{self._name} is jumping")
+
+#Testing
+mammals = [
+    Dog(name="Max", age=5, breed="Golden Retriever"),
+    Cat(name="Pitchup", age=3, color="Black"),
+] 
+for mammal in mammals:
+    mammal.make_sound()
+    mammal.move()
