555e74bb4839ae3f6f7c2bb21f1e8c9b80c5c838
Mouad <mouadelkhou4@gmail.com>
2024-06-29 09:29:27 +0200

    added a new method for futher interaction with the user

--- main.py
+++ main.py
@@ -1,95 +1,138 @@
 class Mammal:
     def __init__(self, name, age):
-        self._name = name  
-        self._age = age  
+        self._name = name
+        self._age = age
+
+    def get_name(self):
+        return self._name
+
+    def set_name(self, name):
+        self._name = name
+
+    def get_age(self):
+        return self._age
+
+    def set_age(self, age):
+        self._age = age
 
     def make_sound(self):
         return
 
     def move(self):
-        return 
+        return
+    
+    def action(self):
+        return
 
 class Dog(Mammal):
     def __init__(self, name, age):
         super().__init__(name, age)
 
     def make_sound(self):
-        print(f"{self._name}: Woof")
+        print(f"{self.get_name()}: Woof")
 
     def move(self):
-        print(f"{self._name} is running")
+        print(f"{self.get_name()} is runing")
 
-class Cat(Mammal):
+    def attack(self):
+        print(f"{self.get_name()} attacks")
+
+    def action(self):
+        self.attack()
+
+  
+class Wildcat(Mammal):
     def __init__(self, name, age):
         super().__init__(name, age)
 
     def make_sound(self):
-        print(f"{self._name}: Meow")
+        print(f"{self.get_name()}: Growl")
 
     def move(self):
-        print(f"{self._name} is jumping")
+        print(f"{self.get_name()} is stalking")
+
+    def attack(self):
+        print(f"{self.get_name()}'s attack: pounces")
+
+    def action(self):
+        self.attack()
+
 
 class Horse(Mammal):
     def __init__(self, name, age):
         super().__init__(name, age)
 
     def make_sound(self):
-        print(f"{self._name}: Neigh")
+        print(f"{self.get_name()}: Neigh")
 
     def move(self):
-        print(f"{self._name} is galloping")
+        print(f"{self.get_name()} is galloping")
+
+    def defend(self):
+        print(f"{self.get_name()}'s defense: kicks")
+
+    def action(self):
+        self.defend()    
 
 class Cow(Mammal):
     def __init__(self, name, age):
         super().__init__(name, age)
 
     def make_sound(self):
-        print(f"{self._name}: Moo")
+        print(f"{self.get_name()}: Moooo")
 
     def move(self):
-        print(f"{self._name} is walking")
+        print(f"{self.get_name()} is walking")
+
+    def defend(self):
+        print(f"{self.get_name()}'s defense: uses its horns")
+
+    def action(self):
+        self.defend()    
 
 class Elephant(Mammal):
     def __init__(self, name, age):
         super().__init__(name, age)
 
     def make_sound(self):
-        print(f"{self._name}: Trumpets")
+        print(f"{self.get_name()}: Trumpets")
 
     def move(self):
-        print(f"{self._name} is lumbering")
+        print(f"{self.get_name()} is lumbering")
 
-class Wildcat(Mammal):
-    def __init__(self, name, age):
-        super().__init__(name, age)
+    def defend(self):
+        print(f"{self.get_name()}'s defense: uses its trunk")
 
-    def make_sound(self):
-        print(f"{self._name}: Growls")
-
-    def move(self):
-        print(f"{self._name} is prowling")
+    def action(self):
+        self.defend()    
 
 class Lion(Mammal):
     def __init__(self, name, age):
         super().__init__(name, age)
 
     def make_sound(self):
-        print(f"{self._name}: Roar")
+        print(f"{self.get_name()}: Roar")
 
     def move(self):
-        print(f"{self._name} is prowling")
+        print(f"{self.get_name()} is prowling")
 
-# Testing
+    def attack(self):
+        print(f"{self.get_name()}'s attack: pounce")
+
+    def action(self):
+        self.attack()    
+
+#testing
 mammals = [
-    Lion(name="Mouad", age=5),
     Dog(name="Max", age=5),
     Wildcat(name="Pitchup", age=3),
     Horse(name="Spirit", age=7),
     Cow(name="Tanoni", age=4),
     Elephant(name="Dabdob", age=10),
+    Lion(name="Mouad", age=5),
 ] 
 
 for mammal in mammals:
-    print(f"Name: {mammal._name}, Age: {mammal._age}")
     mammal.make_sound()
-    mammal.move()
+    mammal.move()   
+    mammal.action()
