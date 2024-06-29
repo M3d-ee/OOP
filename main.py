b45731d7fed418eefafa0053c8caf7a56280212e
Mouad <mouadelkhou4@gmail.com>
2024-06-29 22:55:27 +0200

    added some classes and an interaction with the user

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 class Mammal:
     def __init__(self, name, age):
         self._name = name
@@ -24,15 +26,24 @@ class Mammal:
     def action(self):
         return
 
+    def domestication_status(self):
+        return
+
+    def fight(self, other):
+        print(f"{self.get_name()} is fighting {other.get_name()}")
+        result = random.choice([self, other])
+        print(f"The winner is {result.get_name()}")
+        return result
+
 class Dog(Mammal):
     def __init__(self, name, age):
         super().__init__(name, age)
 
     def make_sound(self):
-        print(f"{self.get_name()}: Woof")
+        print(f"{self.get_name()} (dog): Woof")
 
     def move(self):
-        print(f"{self.get_name()} is runing")
+        print(f"{self.get_name()} is running")
 
     def attack(self):
         print(f"{self.get_name()} attacks")
@@ -40,13 +51,15 @@ class Dog(Mammal):
     def action(self):
         self.attack()
 
-  
+    def domestication_status(self):
+        return "domesticated"
+
 class Wildcat(Mammal):
     def __init__(self, name, age):
         super().__init__(name, age)
 
     def make_sound(self):
-        print(f"{self.get_name()}: Growl")
+        print(f"{self.get_name()} (wildcat): Growl")
 
     def move(self):
         print(f"{self.get_name()} is stalking")
@@ -57,13 +70,15 @@ class Wildcat(Mammal):
     def action(self):
         self.attack()
 
+    def domestication_status(self):
+        return "not domesticated"
 
 class Horse(Mammal):
     def __init__(self, name, age):
         super().__init__(name, age)
 
     def make_sound(self):
-        print(f"{self.get_name()}: Neigh")
+        print(f"{self.get_name()} (horse): Neigh")
 
     def move(self):
         print(f"{self.get_name()} is galloping")
@@ -72,14 +87,17 @@ class Horse(Mammal):
         print(f"{self.get_name()}'s defense: kicks")
 
     def action(self):
-        self.defend()    
+        self.defend()
+
+    def domestication_status(self):
+        return "domesticated"
 
 class Cow(Mammal):
     def __init__(self, name, age):
         super().__init__(name, age)
 
     def make_sound(self):
-        print(f"{self.get_name()}: Moooo")
+        print(f"{self.get_name()} (cow): Moooo")
 
     def move(self):
         print(f"{self.get_name()} is walking")
@@ -88,14 +106,17 @@ class Cow(Mammal):
         print(f"{self.get_name()}'s defense: uses its horns")
 
     def action(self):
-        self.defend()    
+        self.defend()
+
+    def domestication_status(self):
+        return "domesticated"
 
 class Elephant(Mammal):
     def __init__(self, name, age):
         super().__init__(name, age)
 
     def make_sound(self):
-        print(f"{self.get_name()}: Trumpets")
+        print(f"{self.get_name()} (elephant): Trumpets")
 
     def move(self):
         print(f"{self.get_name()} is lumbering")
@@ -104,14 +125,17 @@ class Elephant(Mammal):
         print(f"{self.get_name()}'s defense: uses its trunk")
 
     def action(self):
-        self.defend()    
+        self.defend()
+
+    def domestication_status(self):
+        return "not domesticated"
 
 class Lion(Mammal):
     def __init__(self, name, age):
         super().__init__(name, age)
 
     def make_sound(self):
-        print(f"{self.get_name()}: Roar")
+        print(f"{self.get_name()} (lion): Roar")
 
     def move(self):
         print(f"{self.get_name()} is prowling")
@@ -120,7 +144,124 @@ class Lion(Mammal):
         print(f"{self.get_name()}'s attack: pounce")
 
     def action(self):
-        self.attack()    
+        self.attack()
+
+    def domestication_status(self):
+        return "not domesticated"
+
+class Dolphin(Mammal):
+    def __init__(self, name, age):
+        super().__init__(name, age)
+
+    def make_sound(self):
+        print(f"{self.get_name()} (dolphin): Click-click")
+
+    def move(self):
+        print(f"{self.get_name()} is swimming")
+
+    def defend(self):
+        print(f"{self.get_name()}'s defense : by using its speed")
+
+    def action(self):
+        self.defend()
+
+    def domestication_status(self):
+        return "domesticated"
+
+class Bat(Mammal):
+    def __init__(self, name, age):
+        super().__init__(name, age)
+
+    def make_sound(self):
+        print(f"{self.get_name()} (bat): Screech")
+
+    def move(self):
+        print(f"{self.get_name()} is flying")
+
+    def attack(self):
+        print(f"{self.get_name()} attacks using its sharp teeth")
+
+    def action(self):
+        self.attack()
+
+    def domestication_status(self):
+        return "not domesticated"
+
+class Kangaroo(Mammal):
+    def __init__(self, name, age):
+        super().__init__(name, age)
+
+    def make_sound(self):
+        print(f"{self.get_name()} (kangaroo): Chortle")
+
+    def move(self):
+        print(f"{self.get_name()} is hopping")
+
+    def defend(self):
+        print(f"{self.get_name()}'s defense : uses its powerful legs")
+
+    def action(self):
+        self.defend()
+
+    def domestication_status(self):
+        return "not domesticated"
+
+class Monkey(Mammal):
+    def __init__(self, name, age):
+        super().__init__(name, age)
+
+    def make_sound(self):
+        print(f"{self.get_name()} (monkey): Chatter")
+
+    def move(self):
+        print(f"{self.get_name()} is climbing")
+
+    def attack(self):
+        print(f"{self.get_name()} attacks by throwing objects")
+
+    def action(self):
+        self.attack()
+
+    def domestication_status(self):
+        return "not domesticated"
+
+class Whale(Mammal):
+    def __init__(self, name, age):
+        super().__init__(name, age)
+
+    def make_sound(self):
+        print(f"{self.get_name()} (whale): Sings")
+
+    def move(self):
+        print(f"{self.get_name()} is swimming")
+
+    def defend(self):
+        print(f"{self.get_name()}'s defense : uses its massive tail")
+
+    def action(self):
+        self.defend()
+
+    def domestication_status(self):
+        return "not domesticated"
+
+class Bear(Mammal):
+    def __init__(self, name, age):
+        super().__init__(name, age)
+
+    def make_sound(self):
+        print(f"{self.get_name()} (bear): Growl")
+
+    def move(self):
+        print(f"{self.get_name()} is lumbering")
+
+    def attack(self):
+        print(f"{self.get_name()} attacks with its powerful claws")
+
+    def action(self):
+        self.attack()
+
+    def domestication_status(self):
+        return "not domesticated"
 
 #testing
 mammals = [
@@ -130,9 +271,33 @@ mammals = [
     Cow(name="Tanoni", age=4),
     Elephant(name="Dabdob", age=10),
     Lion(name="Mouad", age=5),
-] 
+    Dolphin(name="Rwiyssi", age=6),
+    Bat(name="Shuho", age=2),
+    Kangaroo(name="Adam", age=3),
+    Monkey(name="Tanoni", age=4),
+    Whale(name="Ghwis", age=15),
+    Bear(name="Daoudi", age=8)
+]
 
 for mammal in mammals:
     mammal.make_sound()
-    mammal.move()   
+    mammal.move()
     mammal.action()
+    print(f"{mammal.get_name()} is {mammal.domestication_status()}.")
+
+#user's interaction
+def choose_and_fight(mammals):
+    for idx, mammal in enumerate(mammals):
+        print(f"{idx}: {mammal.get_name()}")
+
+    first_choice = int(input("Choose the number of the first mammal to fight: "))
+    second_choice = int(input("Choose the number of the second mammal to fight: "))
+
+    if 0 <= first_choice < len(mammals) and 0 <= second_choice < len(mammals):
+        first_mammal = mammals[first_choice]
+        second_mammal = mammals[second_choice]
+        first_mammal.fight(second_mammal)
+    else:
+        print("Invalid choices. Please choose valid mammal numbers.")
+
+choose_and_fight(mammals)
